8536f6b532f6bccabc9128a3b5995f8ead177a9d
Jonathan Slauter <jonathanslauter@gmail.com>
2020-11-25 15:04:18 -0800

    added modules

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-const express = require ("express")
+const express = require("express")
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const app = express()
+const path = require('path')
 
-dotenv.config({path:'config.env'})
+dotenv.config({
+  path: 'config.env'
+})
 const PORT = process.env.PORT || 8080
 
 //log requests
@@ -12,6 +15,19 @@ app.use(morgan('dev'))
 app.get("/", (req, res) => {
   res.send("App running")
 })
+
+//Parsing
+app.use(express.static("public"));
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
+
+//set views engine
+app.set('view engine', 'ejs')
+
+//load assets
+app.use('/css', express.static(path.resolve(__dirname,'assets/css')))
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
